1cd5d5c635d4ae5b79f4c76978db2ebfcdc4286a
cheickBounama <aqadzated@gmail.com>
2024-10-31 16:39:19 +0100

    Ajout des repertoirs de la documentation sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PROJETGiTHUB'
+copyright = "2024, Cheick N'DIAYE et Herman NZEVIETT"
+author = "Cheick N'DIAYE et Herman NZEVIETT"
+release = 'Version 1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
---
+++ docs/source/content/installation.rst
@@ -0,0 +1,11 @@
+.. _installation:
+
+Installation
+============
+
+Pour installer le projet, exécutez la commande suivante :
+
+.. code-block:: bash
+
+   pip install mon_projet
+ 
